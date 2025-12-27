9c522e8de0e5aef4d152cd6f47ec61af45412ed8
Samer Attrah <samiratra95@gmail.com>
2025-12-27 13:36:23 +0300

    feat: Add Meta careers job board to presets
    Includes the Meta careers job board URL to the list of preset job boards for expanded job search capabilities.

--- constants.ts
+++ constants.ts
@@ -3,4 +3,5 @@ export const PRESET_JOB_BOARDS = [
   "https://apply.careers.microsoft.com/",
   "https://jobs.apple.com/en-il/search?location=israel-ISR",
   "https://openai.com/careers/search/",
+  "https://www.metacareers.com/jobsearch",
 ];
